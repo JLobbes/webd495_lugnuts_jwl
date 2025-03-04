29648ee163acb9ae848b44a7b55df374fe5bb45b
Jared Lobbestael <jlobbes2@emich.edu>
2025-03-04 03:47:09 +0000

    ADD: connection toMariaDB SQL database lugnuts_db

--- project/lib/db.js
+++ project/lib/db.js
@@ -1,5 +1,6 @@
 import mysql from 'mysql2';
 
+// a pool is a set of resuable DBs as opposed to single instance connections
 const pool = mysql.createPool({
   host: 'localhost',        // local for now, ClearDB after Deployment??
   user: 'root',             // on my machine
