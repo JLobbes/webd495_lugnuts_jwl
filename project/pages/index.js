d4f3e40dcbca4228ae3b9b51e684daac44d85f2d
Jared Lobbestael <jlobbes2@emich.edu>
2025-04-15 01:10:26 +0000

    ADD: welcome elements to index.js

--- project/pages/index.js
+++ project/pages/index.js
@@ -1,14 +1,52 @@
-// pages/index.js
-
 import Nav from '../components/nav';
 import Footer from '../components/footer';
+import checkAuth from '../hooks/checkAuth';
+import styles from '../styles/home.module.css';
+import { FaSearch, FaReceipt, FaUserPlus } from 'react-icons/fa';
 
-export default function Home() {
+const Home = () => {
+  const user = checkAuth();
+
   return (
     <>
       <Nav />
-      
+      <main className={styles.main}>
+        <h1 className={styles.welcome}>Welcome!</h1>
+        <div className={styles.tileContainer}>
+          {user ? (
+            <>
+              <a href="/product_search" className={styles.tile}>
+                <FaSearch className={styles.icon} />
+                Find a Part
+              </a>
+              <a href="/user_orders" className={styles.tile}>
+                <FaReceipt className={styles.icon} />
+                My Orders
+              </a>
+              <a className={`${styles.tile} ${styles.newsTile}`}>
+                <span className={styles.newsOverlay}>The Latest News</span>
+              </a>
+            </>
+          ) : (
+            <>
+              <a href="/signin_ui" className={styles.tile}>
+                <FaUserPlus className={styles.icon} />
+                Sign In
+              </a>
+              <a href="/product_search" className={styles.tile}>
+                <FaSearch className={styles.icon} />
+                Find a Part
+              </a>
+              <a className={`${styles.tile} ${styles.newsTile}`}>
+                <span className={styles.newsOverlay}>The Latest News</span>
+              </a>
+            </>
+          )}
+        </div>
+      </main>
       <Footer />
     </>
   );
-}
+};
+
+export default Home;
